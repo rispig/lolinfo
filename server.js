3379f6af3399ab694ac52e7538059b66b84888dd
Omri <Omri@Summoners-Rift.local>
2017-12-20 20:02:39 +0200

    API KEY + Get basic info

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const env = require('node-env-file');
 const path = require('path');
 const express = require('express');
 const webpack = require('webpack');
@@ -26,21 +27,57 @@ const middleware = webpackMiddleware(compiler, {
   }
 });
 
+env(__dirname + '/.env');
+
+const apiKeys = [process.env.API_KEY, process.env.ALT_API_KEY];
+const lastKeyUsed = 0;
+const getAPIKey = function () {
+  return apiKeys[~lastKeyUsed + 2];
+}
+
 app.use(middleware);
 app.use(webpackHotMiddleware(compiler));
 app.use(express.static(__dirname));
 
 app.get('/summonerInfo', async (req, res, next) => {
-  const summonerInfoResponse = await axios.get('https://euw1.api.riotgames.com/lol/summoner/v3/summoners/by-name/N00B?api_key=RGAPI-d340ff04-c8a2-4765-a35b-933eff23b951');
+  const {server, summoner} = req.query;
+
+  const summonerInfoResponse = await axios.get(`https://${server}.api.riotgames.com/lol/summoner/v3/summoners/by-name/${summoner}?api_key=${getAPIKey()}`);
+
+  const summonerRankInfo = await axios.get(`https://${server}.api.riotgames.com/lol/league/v3/positions/by-summoner/${summonerInfoResponse.data.id}?api_key=${getAPIKey()}
+  `);
+
+  const soloQ = summonerRankInfo.data.filter(q => q.queueType === 'RANKED_SOLO_5x5')[0];
+  const rank = `${soloQ.tier} ${soloQ.rank}`;
+
+  const realmsResponse = await axios.get(`https://${server}.api.riotgames.com/lol/static-data/v3/realms?api_key=${getAPIKey()}
+  `)
+
+  const profileImagesResponse = await axios.get(`https://${server}.api.riotgames.com/lol/static-data/v3/profile-icons?locale=en_US&api_key=${getAPIKey()}`)
+
+  const summonerJSON = {
+    name: summonerInfoResponse.data.name,
+    level: summonerInfoResponse.data.summonerLevel,
+    rank: rank,
+    profileIcon: {
+      url: `${realmsResponse.data.cdn}/${realmsResponse.data.n.profileicon}/img/sprite/${profileImagesResponse.data.data[summonerInfoResponse.data.profileIconId].image.sprite}`,
+      x: profileImagesResponse.data.data[summonerInfoResponse.data.profileIconId].image.x,
+      y: profileImagesResponse.data.data[summonerInfoResponse.data.profileIconId].image.y,
+      w: profileImagesResponse.data.data[summonerInfoResponse.data.profileIconId].image.w,
+      h: profileImagesResponse.data.data[summonerInfoResponse.data.profileIconId].image.h
+    }
+  }
 
   res.setHeader('Content-Type', 'application/json');
-  res.send(summonerInfoResponse.data);
+  res.send(summonerJSON);
 });
 
-app.listen(port, '0.0.0.0', function onStart(err) {
+
+app.listen(port, '0.0.0.0', async function onStart(err) {
   if (err) {
     console.log(err);
   }
+
   console.info('==> 🌎 Listening on port %s. Open up http://0.0.0.0:%s/ in your browser.', port, port);
 });
 
--- src/components/LolInfo.vue
+++ src/components/LolInfo.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="container">
-    <div class="profile-container">{{ summonerInfo }}</div>
+    <div class="h-container" v-if="summonerInfo">
+      <div v-if="profileIconStyle" :style="profileIconStyle"></div>
+      <div class="v-container name-rank-container">
+        <div>{{ summonerInfo.name }}</div>
+        <div>{{ summonerInfo.rank }}</div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -10,6 +16,7 @@ export default {
   data() {
     return {
       summonerInfo: null,
+      profileIconStyle: null,
     };
   },
   created() {
@@ -17,12 +24,28 @@ export default {
   },
   methods: {
     async getSummonerInfo() {
-      this.summonerInfo = await (await fetch('/summonerInfo')).json();
+      this.summonerInfo = await (await fetch('/summonerInfo?summoner=N00B&server=euw1')).json();
+
+      this.profileIconStyle = {
+        background: `url(${this.summonerInfo.profileIcon.url}) ${this.summonerInfo.profileIcon.x}px ${this.summonerInfo.profileIcon.y}px`,
+        height: `${this.summonerInfo.profileIcon.h}px`,
+        width: `${this.summonerInfo.profileIcon.w}px`,
+      };
     },
   },
 };
 </script>
 
 <style scoped>
+  .h-container, .v-container {
+    display: flex;
+  }
 
+  .v-container {
+    flex-direction: column;
+  }
+
+  .name-rank-container > div {
+    flex: 1;
+  }
 </style>
